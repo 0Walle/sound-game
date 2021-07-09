2ef14ea0d660f12476bebb8292850990c3e186ca
wallegame <wllabferreira@gmail.com>
2021-07-09 18:37:11 -0300

    First commit

---
+++ js/util.js
@@ -0,0 +1,38 @@
+const Note = {
+	C: 261.6,
+	C_: 277.2,
+	D: 293.7,
+	Eb: 311.1,
+	E: 329.6,
+	F: 349.2,
+	F_: 370.0,
+	G: 392.0,
+	G_: 415.3,
+	A: 440.0,
+	Bb: 466.2,
+	B: 493.9,
+}
+
+const noteSets = [
+	[Note.A, Note.B, Note.G],
+	[Note.D, Note.E, Note.F_],
+	[Note.F, Note.G, Note.A],
+	[Note.C, Note.D, Note.E],
+]
+
+function xorshift(seed) {
+	let x = seed
+	x ^= x << 13
+	x ^= x >> 17
+	x ^= x << 5
+	return Math.abs(x)
+}
+
+function shuffleArray(array, rand) {
+	for (let i = array.length - 1; i > 0; i--) {
+		let j = Math.floor(rand() * (i + 1));
+		let temp = array[i];
+		array[i] = array[j];
+		array[j] = temp;
+	}
+}
